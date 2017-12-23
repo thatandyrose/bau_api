57569b8e445487497a0deff6dc5ff8fdafb72149
Andy <thatandyrose@gmail.com>
2017-12-23 07:16:42 +0000

    added seed and fixed json

--- app/views/rota_assignments/_rota_assignment.json.jbuilder
+++ app/views/rota_assignments/_rota_assignment.json.jbuilder
@@ -1,3 +1,5 @@
 json.extract! rota_assignment, :id, :slot, :date, :developer_id, :created_at, :updated_at
 
-json.developer json.extract! rota_assignment.developer, :id, :name, :image_url
+json.developer do
+  json.extract! rota_assignment.developer, :id, :name, :image_url
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |i|
+  Developer.create! name: Faker::Name.name, image_url: "https://bau-api.herokuapp.com/download-#{i}.jpg"
+end
